aeff1f79f1b35bbb1300ce007e97939a72597bea
Gits Dev <gitaumoses380@gmail.com>
2024-05-10 02:42:26 +0300

    chore: replace a tags with react links

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, createBrowserRouter,  createRoutesFromElements, RouterProvider} from "react-router-dom"
-import HomePage from "./pages/HomePage"
 import MainLayout from "./layouts/MainLayout"
+import HomePage from "./pages/HomePage"
+import JobsPage from "./pages/JobsPage"
+import NotFoundPage from "./pages/NotFoundPage"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
-      <Route path="/home" element={<HomePage />} />,
+      <Route index element={<HomePage />} />,
+      <Route path="/jobs" element={<JobsPage />} />,
+      <Route path="*" element={<NotFoundPage />}/>
     </Route>
     // <Route path="/jobs" element={<JobListings />} />,
     // <Route path="/view-all-jobs" element={<ViewAllJobs />} />,
