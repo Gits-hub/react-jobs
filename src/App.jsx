dc31d824bb98f8f8ef252025bbdf4f90c02f6c78
Gits Dev <gitaumoses380@gmail.com>
2024-05-12 01:56:56 +0300

    chore: add addJob page and a useNavigate hook to redirect page after submitting

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,25 @@ import HomePage from "./pages/HomePage"
 import JobsPage from "./pages/JobsPage"
 import JobPage, { jobLoader } from "./pages/JobPage"
 import NotFoundPage from "./pages/NotFoundPage"
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />,
-      <Route path="/jobs" element={<JobsPage />} />,
-      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />, //* A dynamic route with a React Data Loader function. 
-      <Route path="*" element={<NotFoundPage />}/>
-    </Route>
-  )
-)
+import AddJobPage from "./pages/AddJobPage"
 
 const App = () => {
+
+  const addJob = (job) =>{
+    console.log(job)
+  }
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />,
+        <Route path="/jobs" element={<JobsPage />} />,
+        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />, //* A dynamic route with a React Data Loader function. 
+        <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />}/>
+        <Route path="*" element={<NotFoundPage />}/>
+      </Route>
+    )
+  )
 
   return <RouterProvider router={router} />
 
